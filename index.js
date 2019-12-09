fa47d3a846ce254041cd36ee6a77ef554620f36b
gezhonghao <gezhonghao@innotechx.com>
2019-12-09 17:21:19 +0800

    vi

--- index.js
+++ index.js
@@ -33,6 +33,8 @@ http.createServer((req,res)=>{
  * docker 安装
  * ce -> https://docs.docker.com/v18.09/install/linux/docker-ce/ubuntu
  * 
+ * 
  * sudo su  -> 普通用户切换成root用户
  * root用户切成为普通用户  ->  su xxxx
+ * vi xxx.js :set num 显示行号
  */
